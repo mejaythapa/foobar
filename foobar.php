7a22f1932cab40308b74f8deae54f2b66511ee53
jaythapa <jthapa.111it@gmail.com>
2024-03-17 20:54:56 +1100

    comment the logic

--- foobar.php
+++ foobar.php
@@ -1,15 +1,30 @@
 <?php
+/**
+ * Iterates through numbers from 1 to 100 and prints "foo" for numbers divisible by 3,
+ * "bar" for numbers divisible by 5, and "foobar" for numbers divisible by both 3 and 5.
+ *
+ * @return void
+ */
 for ($i = 1; $i <= 100; $i++) {
     $output = '';
+
+    // Check if the number is divisible by 3 and append 'foo' to $output if true
     if ($i % 3 === 0) {
         $output .= 'foo';
     }
+
+    // Check if the number is divisible by 5 and append 'bar' to $output if true
     if ($i % 5 === 0) {
         $output .= 'bar';
     }
+
+    // If $output is empty, print the number; otherwise, print $output
     echo ($output !== '') ? $output : $i;
+
+    // Add a comma and space if it's not the last number
     if ($i < 100) {
         echo ', ';
     }
 }
+
 ?>
